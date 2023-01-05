b0e05b9aea8d5625057a2eb8ed6716e695ff5911
pamutator <akinwumialalade@gmail.com>
2023-01-05 05:56:09 +0100

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if chr(i) == 'e' or chr(i) == 'q':
-        continue
-
-    print("{}".format(chr(i)), end="")
+for ch in range(97, 123):
+        if ch != 101 and ch != 113:
+                print("{:c}".format(ch), end='')
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,1 +1,1 @@
-new readme
+This is the description of 0x01-python-if_else_loops_functions
